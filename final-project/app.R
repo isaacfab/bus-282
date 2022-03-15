b95b4adc36d335d575b0339029a017da323a868c
Isaac Faber <isaacfab@gmail.com>
2022-03-15 08:56:18 -0400

    adding final project

---
+++ final-project/app.R
@@ -0,0 +1,160 @@
+#########################################################################################
+# Final project web app
+# This is a completed application that is ready for hosting
+# this goal of seeing this process is to tie together all
+# of the components we have seen over the course
+# 1. data pipelines provide data that we can then store for later use
+# 2. we can use that data for insight and model development
+# 3. those models can then be used for decision support 
+# In this case it is helping with the question should the
+# winemaker harvest?
+# don't worry about the details behind each component of this tool
+# instead see how a fully developed concept can be deployed and available
+# for an end user to leverage.
+########################################################################################
+
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+########################################################################################
+# To host a Shiny App, we need to use the shiny packages
+########################################################################################
+
+library(shiny)
+library(shinydashboard)
+library(plyr)
+library(tidyverse)
+library(ggplot2)
+
+########################################################################################
+# Reading in our prediction data to the application
+########################################################################################
+
+prediction <- readRDS("data/prediction.RData")
+
+########################################################################################
+# Shiny applications have 2 main components, the UI and the logic
+# Here we define our UI and use the package Shiny daschboards to help create everything
+########################################################################################
+
+ui <- dashboardPage(skin = "green",
+                    dashboardHeader(title = "Decision Support Tool Winemakers Dilemma"),
+                    dashboardSidebar(
+                      collapsed = TRUE,
+                      sidebarMenu(
+                        menuItem("Decision App", tabName = "decision", icon = icon("dashboard"))
+                      )
+                    ),
+                    dashboardBody(
+                      tabItem(tabName = "decision",
+                              # Here is an example viz with the controls of interest
+                              # you can change this as much as you like
+                              fluidRow(
+                                tabBox(width=4,
+                                       #title = "Inputs", #status = "success", #solidHeader = TRUE,
+                                       tabPanel("Probabilities", 
+                                                sliderInput("PRCP", "Precipitation last week", 0,2,0.5, step = 0.1),
+                                                sliderInput("TMAX", "Max temperature last week", 0,100,65, step = 5),
+                                                sliderInput("mold", "Probability of mold given Storm", 0,1,0.4, step = 0.1)
+                                       ),
+                                       tabPanel("Other",
+                                                sliderInput("cost25", "Retail Price per bottle with 25% sugar", 0,50,35, step = 5),
+                                                sliderInput("cost20", "Retail Price per bottle with 20% sugar", 0,50,30, step = 5),
+                                                sliderInput("cost19", "Retail Price per bottle with 19% sugar", 0,50,25, step = 5),
+                                                sliderInput("costbot", "Retail Price per bottle Late Harvest", 0,100,80, step = 10),
+                                                sliderInput("rep_plus", "Reputation Increase from Late Harvest Wine", 0,500000,150000, step = 50000)
+                                                #sliderInput("prob_19", "Probability Obtaining less than 19% sugar without a Storm", 0,1,0.2, step = 0.1),
+                                                #sliderInput("cases", "Number of cases normally made", 500,1500,1000, step = 100)
+                                       )
+                                ),
+                                box(width=8,
+                                    title = "Results", status = "success", solidHeader = TRUE,
+                                    plotOutput("distPlot"),
+                                    HTML(paste("<center><font size = 6>",textOutput('text'), "</font></center>")))
+                              )
+                      )
+                    )
+                    
+)
+
+########################################################################################
+# Shiny applications have 2 main components, the UI and the logic
+# Here we define our server logic, we do all calculations and plots in this section
+########################################################################################
+
+server <- function(input, output) {
+  
+output$distPlot <- renderPlot({
+   
+    #a simple variable for the best alternative 
+    winner <- plot_dataframe() %>% 
+                filter(Best==TRUE)
+    
+    #Create a nice plot for the application
+    ggplot(data = plot_dataframe(), aes(x=Name, y=Value, fill = Best)) +
+      geom_hline(yintercept = 0, colour = "black") +
+      geom_col() + 
+      scale_fill_manual(values = c('red','#00a65a')) +
+      ylab("Expected Utility") + 
+      xlab("") +
+      labs(title = paste0("The best alternative is to ",winner$Name[1])) +
+      geom_text(aes(label = paste0(round(plot_dataframe()$Value,2),"K")), 
+                size = 5, 
+                position = position_stack(vjust = 0.5), 
+                colour = "white") +
+      theme_minimal() +
+      theme(plot.title = element_text(size=22))
+      
+    
+  })
+
+plot_dataframe <- reactive({
+  df <- data.frame(Value = c(NA,NA), 
+                   Name = c("Harvest Now", "Harvest Later"))
+  bot_case <- 12
+  price_harvest_now <- 28.5
+  bot_red <- 0.7
+  thin_aug <- 1.075
+  thin_price <- 10
+  prob_25 <- 0.4
+  prob_20 <- 0.4
+  prob_19 <- 0.2
+  cases <- 1000
+  
+  week40 <- data.frame("PRCPt1" = input$PRCP,
+                       "TMAXt1" = input$TMAX)
+  
+  prob <-predict(prediction, type = 'response', newdata = week40)
+  p_rain <- as.numeric(prob)
+  
+  df[1,1] <- (cases*bot_case*price_harvest_now)/1000
+  
+  lateh_prospect <- ((input$costbot*cases*bot_case*bot_red) + input$rep_plus)/1000
+  thin_prospect <- (thin_aug*thin_price*bot_case*cases)/1000
+  prospect_25 <- input$cost25*bot_case*cases/1000
+  prospect_20 <- input$cost20*bot_case*cases/1000
+  prospect_19 <- input$cost19*bot_case*cases/1000
+  
+  df[2,1] <- (((prospect_25*prob_25) + 
+                 (prospect_20*prob_20) + 
+                 (prospect_19*prob_19))*(1-(p_rain))) +
+                 ((lateh_prospect*input$mold) + 
+                 (thin_prospect*(1-input$mold)))*p_rain
+  
+  df <- df %>% 
+    mutate(Best = ifelse(Value == max(Value), TRUE, FALSE))
+  
+  return(df)
+})
+
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
--- final-project/final-project-starter-script.R
+++ final-project/final-project-starter-script.R
@@ -2,3 +2,149 @@
 # Final Project Starter Script
 # by Isaac J. Faber
 ####################################################
+
+# In this assignment you will build a model and then
+# push it into production within a web application.
+# This script will build the same model from the last
+# assignment.
+
+library(tidyverse)
+library(lubridate)
+library(caTools)
+
+#####################################################################
+# Reading a csv file into R and putting it in data table format
+# Step 1 set the working directory in the session tab above to the 
+# 'To Source File Location'
+#####################################################################
+
+weather <- read.csv("data/Vineyard_weather_1948-2017.csv")
+
+weather$PRCP <- as.numeric(weather$PRCP)
+
+#these next commands replaces missing values with an average of the rest of the columns
+weather$PRCP = ifelse(is.na(weather$PRCP),
+                      ave(weather$PRCP, FUN = function(x) mean(x, na.rm = TRUE)),
+                      weather$PRCP)
+
+weather$RAIN = ifelse((( weather$PRCP > 0 ) & ( is.na(weather$RAIN ))), TRUE, weather$RAIN)
+
+####################################################################
+# Harvest is in september
+# So we want to separate the date field into different fields
+# This will allow us to the calculate weekly statistics
+#####################################################################
+
+weather$DAY <- day(weather$DATE)
+weather$MONTH <- month(weather$DATE)
+weather$YEAR <- year(weather$DATE)
+weather$WEEK <- week(weather$DATE)
+
+#####################################################################
+# We now have a column for Day, Month, Year and Week
+# How should we think about accumulating rows
+# The final output should be a row per week
+#####################################################################
+
+weekly_weather <- weather %>%
+  group_by(YEAR, WEEK)  %>%
+  summarise(PRCP = sum(PRCP), 
+            TMAX = max(TMAX), 
+            TMIN = min(TMIN),
+            RAIN = sum(RAIN))
+
+#####################################################################
+# Our aggregation by week has converted our RAIN column to numeric
+# If we want a categorical value, we must pick a way to measure it
+# Lets assume that for the harvest to be impacted PRCP is greater than 0.35 level
+# and TMAX must be less than 80. Let's also change the name of the column to STORM
+#####################################################################
+
+names(weekly_weather)[6]<-"STORM"
+
+weekly_weather$STORM <- ifelse(weekly_weather$PRCP >= 0.35 & weekly_weather$TMAX<=80, 1, 0)
+
+#####################################################################
+# We know that the harvest happens usually in the 40th week of the year
+# This means we are interested in storms in the 5 weeks prior to the 40th
+# Lets subset our data to only the relevant fields
+#####################################################################
+
+weekly_weather$WEEK <- as.numeric(weekly_weather$WEEK)
+
+weekly_relevant <- weekly_weather %>% 
+  filter(WEEK >= 35 & WEEK < 40)
+
+#####################################################################
+# What else can we do with all this data
+# Let's build our Machine Learning model for prediction (Logistic Regression)
+# Because we won't know the information for the week in question
+# We need to structure the data set as a time series
+# we want to predict next week based on the last two week's values
+#####################################################################
+
+TS_weather = data.frame("STORM" = NA, 
+                        "PRCPt1"  = NA, 
+                        "TMAXt1" = NA, 
+                        "TMINt1" = NA, 
+                        "PRCPt2" = NA, 
+                        "WEEK" = NA)
+
+for (i in 1:nrow(weekly_weather)){
+  TS_weather[i,1] <- weekly_weather[i,6]
+  if (i == 1){
+    TS_weather[i,2] = 0
+    TS_weather[i,3] = 0
+    TS_weather[i,4] = 0
+    TS_weather[i,5] = 0
+    TS_weather[i,6] = 0
+  }else if (i==2){
+    TS_weather[i,2] = weekly_weather[i-1,3]
+    TS_weather[i,3] = weekly_weather[i-1,4]
+    TS_weather[i,4] = weekly_weather[i-1,5]
+    TS_weather[i,5] = 0
+    TS_weather[i,6] = weekly_weather[i-1,2]
+  }else{
+    TS_weather[i,2] = weekly_weather[i-1,3]
+    TS_weather[i,3] = weekly_weather[i-1,4]
+    TS_weather[i,4] = weekly_weather[i-1,5]
+    TS_weather[i,5] = weekly_weather[i-2,3]
+    TS_weather[i,6] = weekly_weather[i-1,2]
+  }
+}
+
+TS_weather$STORM = factor(TS_weather$STORM, levels = c(0, 1))
+
+#####################################################################
+# The next step is to separate training and test set
+# Lets randomly take 75% of our data as a training set
+#####################################################################
+
+set.seed(123)
+split = sample.split(TS_weather$STORM, SplitRatio = 0.75)
+training_set = subset(TS_weather, split == TRUE)
+test_set = subset(TS_weather, split == FALSE)
+
+
+#####################################################################
+# Now let's create our regression
+# glm means: Generalized Linear Model
+# family = binomial means its a logit regression
+#####################################################################
+
+classifier = glm(formula = STORM ~ PRCPt1 + TMAXt1,
+                 family = binomial,
+                 data = training_set)
+
+#####################################################################
+# Let's save the classifier as an object.
+# This is done so we can call use it elsewhere (like a production app)
+# It comes in handy when hosting a solution that uses an object
+#####################################################################
+
+saveRDS(classifier, file = "data/prediction.RData")
+
+#######################################
+# now go open the app.R file 
+# follow the directions from there
+#######################################
